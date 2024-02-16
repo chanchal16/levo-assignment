9b476de9169e6db3fb9165064c1f898da19bf241
chanchal013 <chanchal@compose.co.in>
2024-02-16 16:41:25 +0530

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="h-full">
+      <h1 className="text-brown">helllooooo</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,jsx,tsx}"],
+  theme: {
+    colors: {
+      cream: "#fffaf0",
+      brown: "#a52a2a",
+      blue: "#7d94b5",
+      black: "#2d2d2d",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
